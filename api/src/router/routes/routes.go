b62fdab55b4e9ca9f8b78d48e34defc07e1faf45
thevalter <valternunes776@gmail.com>
2024-11-23 08:40:58 -0300

    insert routes into Router

--- api/src/router/routes/routes.go
+++ api/src/router/routes/routes.go
@@ -1,10 +1,24 @@
 package routes
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 type Route struct {
 	URI      string
 	Method   string
 	Function func(http.ResponseWriter, *http.Request)
 	NeedAuth bool
+}
+
+func Config(r *mux.Router) *mux.Router {
+	routes := UsersRoutes
+
+	for _, route := range routes {
+		r.HandleFunc(route.URI, route.Function).Methods(route.Method)
+	}
+
+	return r
 }
